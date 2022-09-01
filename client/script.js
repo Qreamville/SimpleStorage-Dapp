8e3e8d7c81d6a12a005546ec727de62a17738b2d
Qreamville <Oyeleyebalikis@yahoo.com>
2022-09-01 02:12:15 -1200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,22 +1,53 @@
 import { ethers } from "./ether-5.6.esm.min.js";
+import { abi } from "./abi.js";
+
 const connectButton = document.getElementById("connectBtn");
-const retrieve = document.getElementById("retrieve");
+const retrieveButton = document.getElementById("retrieve");
+const showFavoriteNumber = document.getElementById("showFavoriteNumber");
+const storeInput = document.getElementById("store");
+const storeButton = document.getElementById("storeBtn");
+const address = "0x40c0a64a827ab8ccad43e28cbe3f4e867a5ed400";
 
 const connectWallet = async () => {
-  console.log("working");
   if (typeof window.ethereum != "undefined") {
-    window.ethereum.request({ method: "eth_requestAccounts" });
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    connectButton.innerHTML = "Connected!";
   } else {
     alert("Please install Metamask");
   }
 };
-// 0x40c0a64a827ab8ccad43e28cbe3f4e867a5ed400
 
 connectButton.onclick = connectWallet;
+
+const retrieve = async () => {
+  if (typeof window.ethereum != "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, signer);
+
+    try {
+      const getRetrieve = await contract.retrieve();
+      showFavoriteNumber.innerHTML = getRetrieve.toString();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+retrieveButton.onclick = retrieve;
 
 const store = async () => {
   if (typeof window.ethereum != "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
+    const contract = new ethers.Contract(address, abi, signer);
+
+    try {
+      await contract.store(storeInput.value);
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
+
+storeButton.onclick = store;
